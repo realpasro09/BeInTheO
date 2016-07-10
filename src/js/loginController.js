2df9ed2b5875401a78f40cd148c5b043424c086c
erickdc <erickson_523@hotmail.com>
2016-07-10 09:35:25 -0600

    Added dynamic image to circle

--- src/js/loginController.js
+++ src/js/loginController.js
@@ -8,24 +8,51 @@
 
     function LoginCtrl($facebook) {
         var vm = this;
-        vm.isLoggedIn = false;
-        vm.login = function() {
+        vm.circles = [];
+        initializeCircles();
+        vm.login = function(position) {
             console.log("logeandome");
             $facebook.login().then(function() {
-                refresh();
+                refresh(position);
             });
         };
-        function refresh() {
+
+        vm.onMouseOver = onMouseOver;
+        vm.onMouseOut = onMouseOut;
+
+        function refresh(position) {
             $facebook.api("/me/picture?type=normal").then(
                 function(response) {
-                    vm.imageUrl = response.data.url;
-                    vm.isLoggedIn = true;
+                    vm.circles[position].imageUrl = response.data.url;
+                    vm.circles[position].isLoggedIn = true;
+                    vm.circles[position].previousImageUrl = response.data.url;
                 },
                 function(err) {
-                    vm.welcomeMsg = "Please log in";
+
                 });
         }
 
+        function initializeCircles() {
+            for (var i = 0; i < 100; i++)
+                vm.circles.push({imageUrl: 'images/review_05.png', isLoggedIn: false, previousImageUrl: 'images/review_05.png'});
+        }
+
+        function onMouseOver(position)
+        {
+            if(vm.circles[position].isLoggedIn) return;
+            vm.circles[position].previousImageUrl = vm.circles[position].imageUrl;
+            vm.circles[position].imageUrl =  'images/review_06.png';
+        }
+
+        function onMouseOut(position)
+        {
+            if(!vm.circles[position].isLoggedIn)
+            {
+                vm.circles[position].imageUrl =  'images/review_05.png';
+            }else{
+                vm.circles[position].imageUrl =  vm.circles[position].previousImageUrl;
+            }
+        }
         refresh();
     }
 })();
